371278b9f3f0e206f616b96b9658e375e3c49759
RAJAT 28 <rajat.cdev@gmail.com>
2022-12-01 11:30:37 +0530

    second-commit 12

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import Textform from './components/Textform';
 import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743';
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
       {/* <Navbar title="TextUtils" about="About-TextUtils"/> */}
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -37,10 +37,10 @@ export default function Textform(props) {
     
     return (
         <>
-        <div className={`container text-${props.mode==='light' ? 'dark':'light'}`}>
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}} >
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="mybox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To Lowercase</button>
@@ -48,7 +48,7 @@ export default function Textform(props) {
             <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
-        <div className={`container text-${props.mode==='light' ? 'dark':'light'}`}>
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h1>Your Text Summary</h1>
             <p>{text.split(" ").length} Words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes Reading</p>
