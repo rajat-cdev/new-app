da03c2c2811020906abd62913ba73eac8818f4c8
RAJAT 28 <rajat.cdev@gmail.com>
2022-12-01 12:08:46 +0530

    Third commit ERROR 13

--- src/App.js
+++ src/App.js
@@ -3,25 +3,37 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import React, {useState} from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light');
+  const[alert, setAlert] = useState('This is Alert');
+
+  const showAlert = (message, type)=> {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
   const toggleMode = () => {
     if (mode === 'light'){
       setMode('dark')
       document.body.style.backgroundColor = '#042743';
+      // showAlert("Dark mode has been enabled", "success");
     }
     else {
       setMode('light')
       document.body.style.backgroundColor = 'white';
+      // showAlert("Light mode has been enabled", "success");
     }
   }
   return (
     <>
       {/* <Navbar title="TextUtils" about="About-TextUtils"/> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-      <Textform heading="Enter Your Text" mode={mode} />
+      <Textform showAlert={showAlert} heading="Enter Your Text" mode={mode} />
       {/* <About /> */}
       </div>
     </>
